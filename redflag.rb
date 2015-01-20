b6d729252941b44f8188e3b7b8ab5493f24fd0dc
zhulinpinyu <mulixiang199016@gmail.com>
2015-01-21 01:02:44 +0800

    protect instance variables

--- redflag.rb
+++ redflag.rb
@@ -26,6 +26,7 @@ lambda {
 load 'events.rb'
 
 each_event do |event|
-  each_setup { |setup| setup.call }
-  puts "ALERT: #{event[:description]}" if event[:condition].call
+  env = Object.new
+  each_setup { |setup| env.instance_eval &setup }
+  puts "ALERT: #{event[:description]}" if env.instance_eval &(event[:condition])
 end
