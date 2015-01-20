e06e281a65c1d1f8f59ba51b798566d923f19eba
zhulinpinyu <mulixiang199016@gmail.com>
2015-01-20 08:46:19 +0800

    share value

--- events.rb
+++ events.rb
@@ -1,8 +1,14 @@
-event "an event always happens" do
-  true
+def monthly_sales
+  110 #Todo
 end
 
-event "an event that never happens" do
-  false
+target_sales = 100
+
+event "monthly sales are suspiciously high" do
+  monthly_sales > target_sales
+end
+
+event "monthly sales are abysmally low" do
+  monthly_sales < target_sales
 end
 
