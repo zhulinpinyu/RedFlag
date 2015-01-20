7f09bc889d81d52add73775ca231a2e76510fe30
zhulinpinyu <mulixiang199016@gmail.com>
2015-01-21 00:57:37 +0800

    refactor with lambda

--- redflag.rb
+++ redflag.rb
@@ -1,19 +1,31 @@
-def event(description,&block)
-  @events << { description: description, condition: block }
-end
+lambda {
+  events = []
+  setups = []
 
-def setup(&block)
-  @setups << block
-end
+  Kernel.send :define_method, :event do |description,&block|
+    events << {description: description, condition: block}
+  end
 
-@setups = []
-@events = []
+  Kernel.send :define_method, :setup do |&block|
+    setups << block
+  end
+
+  Kernel.send :define_method, :each_setup do |&block|
+    setups.each do |setup|
+      block.call setup
+    end
+  end
+
+  Kernel.send :define_method, :each_event do |&block|
+    events.each do |event|
+      block.call event
+    end
+  end
+}.call
 
 load 'events.rb'
 
-@events.each do |event|
-  @setups.each do |setup|
-    setup.call
-  end
+each_event do |event|
+  each_setup { |setup| setup.call }
   puts "ALERT: #{event[:description]}" if event[:condition].call
 end
